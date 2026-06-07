4868e84fc30f2fff4b4d82ca9e8f56c7848cefa3
CHAUDHARY_SUHANA <suhanachaudhary212@gmail.com>
2026-06-07 22:56:01 +0530

    frontend url11

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,11 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://warehouse-management-system-frontend.onrender.com/"
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
