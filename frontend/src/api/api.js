3feb524f38e0b56041c8cfff12ed8f423367bdfc
CHAUDHARY_SUHANA <suhanachaudhary212@gmail.com>
2026-06-07 22:46:36 +0530

    frontend url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://warehouse-management-system-backend-qro9.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
